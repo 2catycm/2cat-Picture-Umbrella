0e7f87d981dd210f571a88af1f66fd091af4d58c
叶璨铭 <2603119857@qq.com>
2021-05-21 18:19:45 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace 二猫图片伞
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CloseLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Location = new System.Drawing.Point(12, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(819, 435);
+            this.panel1.TabIndex = 0;
+            // 
+            // CloseLabel
+            // 
+            this.CloseLabel.AutoSize = true;
+            this.CloseLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.CloseLabel.Font = new System.Drawing.Font("Fira Code", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CloseLabel.Location = new System.Drawing.Point(773, 9);
+            this.CloseLabel.Name = "CloseLabel";
+            this.CloseLabel.Size = new System.Drawing.Size(59, 31);
+            this.CloseLabel.TabIndex = 2;
+            this.CloseLabel.Text = " x ";
+            this.CloseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CloseLabel.Click += new System.EventHandler(this.CloseLabel_Click);
+            this.CloseLabel.MouseEnter += new System.EventHandler(this.CloseLabel_MouseEnter);
+            this.CloseLabel.MouseLeave += new System.EventHandler(this.CloseLabel_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("楷体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(9, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(758, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "二猫图片伞2.0——二猫工作室出品";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(844, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CloseLabel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label CloseLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 二猫图片伞
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Window = CloseLabel.BackColor;
+            controlText = CloseLabel.ForeColor;
+
+            
+        }
+        private static bool mov = false;//初始化
+        private static int xpos;
+        private static int ypos;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private static Color Window;
+        private static Color controlText;
+        private void CloseLabel_MouseEnter(object sender, EventArgs e)
+        {
+            CloseLabel.BackColor = Color.Red;
+            CloseLabel.ForeColor = Window;
+        }
+        private void CloseLabel_MouseLeave(object sender, EventArgs e)
+        {
+            //Console.WriteLine("hello");
+            CloseLabel.BackColor = Window;
+            CloseLabel.ForeColor = controlText;
+        }
+
+        private void CloseLabel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = true;
+
+            xpos = MousePosition.X;//鼠标的x坐标为当前窗体左上角x坐标
+
+            ypos = MousePosition.Y;//鼠标的y坐标为当前窗体左上角y坐标
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mov)
+
+            {
+
+                this.Left += MousePosition.X - xpos;//根据鼠标x坐标确定窗体的左边坐标x
+
+                this.Top += MousePosition.Y - ypos;//根据鼠标的y坐标窗体的顶部，即Y坐标
+
+                xpos = MousePosition.X;
+
+                ypos = MousePosition.Y;
+
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+
+                mov = false;//停止移动
+
+            
+
+ 
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            xpos = 0; //设置初始状态
+
+            ypos = 0;
+
+            mov = false;
+        }
+    }
+}
